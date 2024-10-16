2e39ee4f0c50ef5d73a8caadfe43bf5a8bc68959
Himawan289 <160473999+Himawan289@users.noreply.github.com>
2024-10-16 23:46:18 +0700

    menambahkan halaman favorite dan history

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 def DashboardPage(request):
     return render(request, 'dashboard.html')  
 
+def DashboardPage(request):
+    return render(request, 'favorite.html')  
